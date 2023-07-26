449b1aeea40ec812a67e35051d8d0c5916e0c139
Riya-Maharjan <riyamaharjan05@gmail.com>
2023-07-26 22:31:52 +0545

    resolve bugs + added remaining commands

--- lib/widgets/dialogs/profile_dialog.dart
+++ lib/widgets/dialogs/profile_dialog.dart
@@ -1,5 +1,6 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:chat_app/models/chat_user.dart';
+import 'package:chat_app/screens/view_profile_screen.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -21,13 +22,12 @@ class ProfileDialog extends StatelessWidget {
           child: Stack(
             children: [
               Positioned(
-                top: mq.height * 0.75,
-                left: mq.width * 0.1,
+                top: mq.height * .08,
+                left: mq.width * .12,
                 child: ClipRRect(
                   borderRadius: BorderRadius.circular(mq.height * 0.3),
                   child: CachedNetworkImage(
-                    width: mq.height * 0.2,
-                    height: mq.height * 0.2,
+                    width: mq.width * 0.5,
                     fit: BoxFit.fill,
                     imageUrl: user.image,
                     // placeholder: (context, url) => const CircularProgressIndicator(),
@@ -39,20 +39,27 @@ class ProfileDialog extends StatelessWidget {
               Positioned(
                 left: mq.width * .04,
                 top: mq.height * .02,
-                width: mq.width * 0.55,
+                width: mq.width * .55,
                 child: Text(
                   user.name,
-                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                  style: const TextStyle(
+                      fontSize: 20, fontWeight: FontWeight.bold),
                 ),
               ),
               Positioned(
                 right: 8,
-                top: 4,
+                top: 6,
                 child: MaterialButton(
-                    onPressed: () {},
-                    shape: CircleBorder(),
+                    onPressed: () {
+                      Navigator.pop(context);
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (_) => ViewProfileScreen(user: user)));
+                    },
+                    shape: const CircleBorder(),
                     minWidth: 0,
-                    padding: EdgeInsets.all(0),
+                    padding: const EdgeInsets.all(0),
                     child: const Icon(
                       Icons.info_outline,
                       size: 30,
